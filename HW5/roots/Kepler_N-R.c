91d4cb058d61b91db126f5907f7307316332d7b4
picoriley <acloudy@mail.smu.edu>
2013-10-04 08:53:43 -0500

    Kepler function outputs something, but I still think that something is wrong.

--- HW5/roots/Kepler_N-R.c
+++ HW5/roots/Kepler_N-R.c
@@ -4,27 +4,29 @@
 #include "roots.h"
 
 double M;
+double e;
 
-double f(double e) {
-  double y;
-  y =   ;  /* Complete this statement */
-  return(y);
+double f(double E) {
+  double f;
+  f =   E - e*sin(E) - M;  /* Complete this statement */
+  return(f);
 }
 
-double fp(double e) {
-  double yp;
-  yp =   ;   /* Complete this statement */
-  return(yp);
+double fp(double E) {
+  double fp;
+  fp =   1 - e*cos(E);   /* Complete this statement */
+  return(fp);
 }
 
 int main(int argc,char *argv[]) {
-  double e;
 
-  if (argc != 2) {
-    fprintf(stderr,"%s <e>\n",argv[0]);
+
+  if (argc != 3) {
+    fprintf(stderr,"%s <e> <M>\n",argv[0]);
     exit(1);
   }
   e = atof(argv[1]);
-  newton_raphson(f,fp,e,100,1.0e-9,1.0e-30);
+  M = atof(argv[2]);
+  newton_raphson(f,fp,M,100,1.0e-9,1.0e-30);
   exit(0);
 }
