bb0acf6a82c9b10b311c301f6c5355ed5e8550f1
picoriley <acloudy@mail.smu.edu>
2013-10-23 18:14:41 -0500

    Testing the autogit program

--- autogit.c
+++ autogit.c
@@ -2,30 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SHELLSCRIPT "\
-#/bin/bash \n\
-echo \"hello\" \n\
-echo \"how are you\" \n\
-echo \"today\" \n\
-"
-
 int main(int argc,char *argv[])
 {
-  if (argc != 2)
+  if (argc < 2 || argc > 3)
   {
-    fprintf(stderr,"%s <Commit Message>\n",argv[0]);
+    fprintf(stderr,"%s [Directory] <Commit Message>\n",argv[0]);
     exit(1);
   }
   
   char str[1000];
-  strcpy(str, "\
-  #/bin/bash \n\
-  git add *\n\
-  git commit -m \"");
-  strcat(str, argv[1]);
-  strcat(str, "\"\n\
-  git push origin master\n\
-  ");
+  
+  if (argc == 2)
+  {
+      
+      strcpy(str, "\
+      #/bin/bash \n\
+      git add -A\n\
+      git commit -m \"");
+      
+      strcat(str, argv[1]);
+      
+      strcat(str, "\"\n\
+      git push origin master\n\
+      ");
+  }
+  else
+  {
+    strcpy(str, "\
+    #/bin/bash \n\
+    git add ");
+
+    strcat(str, argv[1]);
+
+    strcat(str, "*\n\
+    git commit -m \"");
+
+    strcat(str, argv[2]);
+
+    strcat(str, "\"\n\
+    git push origin master\n\
+    ");
+  }
 
   system(str);
   return 0;
